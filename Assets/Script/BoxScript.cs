3e568fe628866a316b37028bd7b6315094d4ae73
dpwqb <99231063+dpwqb@users.noreply.github.com>
2022-12-14 10:09:14 +0800

    Add files via upload

---
+++ Assets/Script/BoxScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+public class BoxScript : MonoBehaviour
+{
+    //存在时间
+    float LiveTime;
+    //闪烁时间
+    float BlingTime;
+    //是否显示
+    bool Display;
+    private void Update()
+    {
+        if((LiveTime+=Time.deltaTime)>8)
+            Destroy(gameObject);
+        else if(LiveTime>5&&(BlingTime+=Time.deltaTime)>0.1f)
+        {
+            BlingTime=0;
+            gameObject.GetComponent<MeshRenderer>().enabled=Display=!Display;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            GameObject.Find("玩家").GetComponent<Player>().GetBox();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject box;
+    public float attackDis;
+    Animator anim;
+    NavMeshAgent nav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player=GameObject.Find("玩家");
+        anim=GetComponent<Animator>();
+        nav=GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.transform.position);
+        if(Vector3.Distance(transform.position,player.transform.position)<=attackDis)
+            anim.SetBool("攻击",true);
+        else
+            anim.SetBool("攻击",false);
+    }
+    public void AttackEvent()
+    {
+        player.GetComponent<Player>().Hurt(5);
+    }
+    public void EnemyDead()
+    {
+        anim.SetTrigger("死");
+        nav.isStopped=true;
+        gameObject.GetComponent<Collider>().enabled=false;
+        Destroy(gameObject,3);
+        //打掉僵尸后生成的宝箱
+        if(Random.Range(0,8)==0)
+        Instantiate(box,transform.position+new Vector3(0,0.25f,0),Quaternion.identity);
+    }
+}
